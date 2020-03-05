4cdf57bb5a35971d31a9fdf63bb30b26020358e4
YeisonTapia <yftapial@ut.edu.co>
2020-03-04 21:22:38 -0500

    Add fields for catetories migration

--- database/migrations/2020_03_05_021525_create_categories_table.php
+++ database/migrations/2020_03_05_021525_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('image');
+            $table->integer('parent_id')->default(0);
             $table->timestamps();
         });
     }
