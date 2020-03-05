898aebdd6a28a4f693df70961f4728e13662a86f
YeisonTapia <yftapial@ut.edu.co>
2020-03-04 21:55:45 -0500

    Add relationships

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function products()
+    {
+        return $this->hasMany('App\Product');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Category', 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('App\Category', 'parent_id');
+    }
 }
