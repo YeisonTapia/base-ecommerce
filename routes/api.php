bdabd180fd35536100e198413cd9d0a40a0b86e3
YeisonTapia <yftapial@ut.edu.co>
2020-03-04 21:42:13 -0500

    Add api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,20 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::middleware([])->group(function () {
+    
+    Route::post('/categories', 'CategoryController@store');
+    Route::get('/categories', 'CategoryController@index');
+    Route::get('/categories/{category}', 'CategoryController@show');
+    Route::put('/categories/{category}', 'CategoryController@update');
+    Route::delete('/categories/{category}', 'CategoryController@destroy');
+
+    Route::post('/produtcs', 'ProductController@store');
+    Route::get('/produtcs', 'ProductController@index');
+    Route::get('/produtcs/{produtc}', 'ProductController@show');
+    Route::put('/produtcs/{produtc}', 'ProductController@update');
+    Route::delete('/produtcs/{produtc}', 'ProductController@destroy');
+
+});
